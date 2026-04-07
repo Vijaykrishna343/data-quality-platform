1858c5074019e7407fca098f3c7f5777fd648692
D.Sai Kiran <128113541+saikiran369369@users.noreply.github.com>
2026-04-07 12:46:50 +0530

    added requiremnts.txt file and changed file_manager.py file

--- backend/core/file_manager.py
+++ backend/core/file_manager.py
@@ -11,13 +11,16 @@ def save_cleaned_file(dataset_id, df):
     return path
 
 
-    # If all encodings fail, let pandas throw the default exception, we catch it locally
+def read_csv(file_path, **kwargs):
     try:
         df = pd.read_csv(file_path, encoding="utf-8", **kwargs)
+    except UnicodeDecodeError:
+        # fallback encoding
+        df = pd.read_csv(file_path, encoding="latin1", **kwargs)
     except Exception as e:
         raise ValueError(f"Corrupted or unsupported dataset format: {str(e)}")
-        
+
     if df.empty:
         raise ValueError("Uploaded dataset is empty")
-        
+
     return df
